60da6aac5311e1ad9e5d8c2436f992a4b1370568
Michael Scheurer <michael.scheurer@mikeysoft.ch>
2024-11-02 17:49:23 +0100

    add Daily ststistic

--- BWT Perla Seta/module.php
+++ BWT Perla Seta/module.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 			$this->RegisterPropertyString("Password", '');
 			$this->RegisterPropertyBoolean("HTTPUpdateTimer", false);
 			$this->RegisterPropertyInteger("UpdateTimer", 10);
+			$this->RegisterPropertyBoolean("Statistik", false);
 			$this->RegisterPropertyBoolean("DebugLog", false);
 
 			// Timer
@@ -36,7 +37,8 @@ declare(strict_types=1);
 
         	// Set Timer
         	if ($this->ReadPropertyBoolean("HTTPUpdateTimer") and $this->ReadPropertyInteger("UpdateTimer") >= 10) {
-            	$this->SetTimerInterval("BWTPerla_UpdateTimer", $this->ReadPropertyInteger("UpdateTimer")*1000);
+//            	$this->SetTimerInterval("BWTPerla_UpdateTimer", $this->ReadPropertyInteger("UpdateTimer")*1000);
+            	$this->SetTimerInterval("BWTPerla_UpdateTimer", 30000);
         	} else {
             	$this->SetTimerInterval("BWTPerla_UpdateTimer", 0);
         	}
@@ -102,23 +104,27 @@ declare(strict_types=1);
 				$this->SetValue("WaterTreatedCurrentMonth_l", $data['WaterTreatedCurrentMonth_l']);
 				$this->SetValue("WaterTreatedCurrentYear_l", $data['WaterTreatedCurrentYear_l']);
 			}
-	
-			// Get Health Data
-			$data = $this->SendHTTPCommand('GetDailyData');
-			if ($data == false) {
-				return false;
-			} elseif (isset($data['successful'])) {#
+			if ($this->ReadPropertyBoolean("Statistik")) {
+				// Get Health Data
+				$data = $this->SendHTTPCommand('GetDailyData');
+				if ($data == false) {
+					IPS_SemaphoreLeave($semaphore);
+					$this->log('Update - Keine Tages Ststistik Daten empfangen');
+					$this->log('Update - Semaphore leaved');
+				} else {
+					$StatistikDayCat = @IPS_GetCategoryIDByName('Verbrauch Tag', $this->InstanceID);
+					$this->SetValue("00:00-00-29", $data['0000_0029_l']);
 //				$this->SetValue("mowerVoltageInternal", $data['health']['voltages']['int3v3']/1000);
 //				$this->SetValue("mowerVoltageExternal", $data['health']['voltages']['ext3v3']);
-//				$this->SetValue("mowerVoltageBattery", $data['health']['voltages']['batt']/1000);
+//				$this->SetValue("mowerVoltageBattery", $data['health']['voltages']['batt']/10
+				}
 			}
-	
 			// Set Timer
-			if ($this->ReadPropertyBoolean("HTTPUpdateTimer") and $this->ReadPropertyInteger("UpdateTimer") >= 10) {
-				$this->SetTimerInterval("BWTPerla_UpdateTimer", $this->ReadPropertyInteger("UpdateTimer")*1000);
-			} else {
-				$this->SetTimerInterval("BWTPerla_UpdateTimer", 0);
-			}
+//			if ($this->ReadPropertyBoolean("HTTPUpdateTimer") and $this->ReadPropertyInteger("UpdateTimer") >= 10) {
+//				$this->SetTimerInterval("BWTPerla_UpdateTimer", $this->ReadPropertyInteger("UpdateTimer")*1000);
+//			} else {
+//				$this->SetTimerInterval("BWTPerla_UpdateTimer", 0);
+//			}
 	
 			IPS_SemaphoreLeave($semaphore);
 			$this->log('Update - Semaphore leaved');
@@ -238,40 +244,159 @@ declare(strict_types=1);
 			$this->RegisterVariableInteger("WaterTreatedCurrentMonth_l", "WaterTreatedCurrentMonth_l", "", 21 );
 			$this->RegisterVariableInteger("WaterTreatedCurrentYear_l", "WaterTreatedCurrentYear_l", "", 21 );
 	
-			//----HTMLBox
-/*
-			if ($this->ReadPropertyBoolean("HtmlBoxElements")) {
-				if (!$HTMLboxCat = @IPS_GetCategoryIDByName('HTMLBox', $this->InstanceID)) {
-					$HTMLboxCat = IPS_CreateCategory();   // Kategorie anlegen
-						IPS_SetName($HTMLboxCat, "HTMLBox");   // Kategorie auf Timer umbenennen
-						IPS_SetParent($HTMLboxCat, $this->InstanceID); // Kategorie Timer einsortieren unter der Robonect Instanz
+			//---- Statistik
+
+			if ($this->ReadPropertyBoolean("Statistik")) {
+				if (!$StatistikDayCat = @IPS_GetCategoryIDByName('Verbrauch Tag', $this->InstanceID)) {
+					$StatistikDayCat = IPS_CreateCategory();   // Kategorie anlegen
+						IPS_SetName($StatistikDayCat, "Verbrauch Tag");   // Kategorie umbenennen
+						IPS_SetParent($StatistikDayCat, $this->InstanceID); // Kategorie einsortieren unter der BWT Instanz
 				}
-				if (!@IPS_GetObjectIDByIdent("Timerlist", $HTMLboxCat)) {
-					IPS_SetParent($this->RegisterVariableString("Timerlist", $this->Translate('Timerlist'), "~HTMLBox", 201), $HTMLboxCat); 
+				if (!@IPS_GetObjectIDByIdent("00:00-00:29", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("00:00-00-29", "00:00-00-29", "", 101), $StatistikDayCat); 
 				}
-				if (!@IPS_GetObjectIDByIdent("Errorlist", $HTMLboxCat)) {
-					IPS_SetParent($this->RegisterVariableString("Errorlist", $this->Translate('Errorlist'), "~HTMLBox", 202), $HTMLboxCat); 
+				if (!@IPS_GetObjectIDByIdent("00:30-00:59", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("00:30-00-59", "00:30-00-59", "", 102), $StatistikDayCat); 
 				}
-				if (!@IPS_GetObjectIDByIdent("Batterylist", $HTMLboxCat)) {
-					IPS_SetParent($this->RegisterVariableString("Batterylist", $this->Translate('Batterylist'), "~HTMLBox", 203), $HTMLboxCat); 
+				if (!@IPS_GetObjectIDByIdent("01:00-01:29", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("01:00-01-29", "01:00-01-29", "", 103), $StatistikDayCat); 
 				}
-				if (!@IPS_GetObjectIDByIdent("TimeStatlist", $HTMLboxCat)) {
-					IPS_SetParent($this->RegisterVariableString("TimeStatlist", $this->Translate('Operation hours list'), "~HTMLBox", 204), $HTMLboxCat); 
+				if (!@IPS_GetObjectIDByIdent("01:30-01:59", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("01:30-01-59", "01:30-01-59", "", 104), $StatistikDayCat); 
 				}
-				if (!@IPS_GetObjectIDByIdent("Drivelist", $HTMLboxCat)) {
-					IPS_SetParent($this->RegisterVariableString("Drivelist", $this->Translate('Drivelist'), "~HTMLBox", 204), $HTMLboxCat); 
+				if (!@IPS_GetObjectIDByIdent("02:00-02:29", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("02:00-02-29", "02:00-02-29", "", 105), $StatistikDayCat); 
 				}
-				if (!@IPS_GetObjectIDByIdent("Versionlist", $HTMLboxCat)) {
-					IPS_SetParent($this->RegisterVariableString("Versionlist", $this->Translate('Versionlist'), "~HTMLBox", 204), $HTMLboxCat);
+				if (!@IPS_GetObjectIDByIdent("02:30-02:59", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("02:30-02-59", "02:30-02-59", "", 106), $StatistikDayCat); 
 				}
-				$this->SetErrorBox();
-				$this->GetBatteryData();
-				$this->SetTimeStatBox();
-				$this->SetDrivesBox();
-				$this->SetVersion();
-				$this->SetWlan();
+				if (!@IPS_GetObjectIDByIdent("03:00-03:29", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("03:00-03-29", "03:00-03-29", "", 107), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("03:30-03:59", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("03:30-03-59", "03:30-03-59", "", 108), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("04:00-04:29", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("04:00-04-29", "04:00-04-29", "", 109), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("04:30-04:59", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("04:30-04-59", "04:30-04-59", "", 110), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("05:00-05:29", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("05:00-05-29", "05:00-05-29", "", 111), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("05:30-05:59", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("05:30-05-59", "05:30-05-59", "", 112), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("06:00-06:29", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("06:00-06-29", "06:00-06-29", "", 113), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("06:30-06:59", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("06:30-06-59", "06:30-06-59", "", 114), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("07:00-07:29", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("07:00-07-29", "07:00-07-29", "", 115), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("07:30-07:59", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("07:30-07-59", "07:30-07-59", "", 116), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("08:00-08:29", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("08:00-08-29", "08:00-08-29", "", 117), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("08:30-08:59", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("08:30-08-59", "08:30-08-59", "", 118), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("09:00-09:29", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("09:00-09-29", "09:00-09-29", "", 119), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("09:30-09:59", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("09:30-09-59", "09:30-09-59", "", 120), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("10:00-10:29", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("10:00-10-29", "10:00-10-29", "", 121), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("10:30-10:59", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("10:30-10-59", "10:30-10-59", "", 122), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("11:00-11:29", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("11:00-11-29", "11:00-11-29", "", 123), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("11:30-11:59", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("11:30-11-59", "11:30-11-59", "", 124), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("12:00-12:29", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("12:00-12-29", "12:00-12-29", "", 125), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("12:30-12:59", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("12:30-12-59", "12:30-12-59", "", 126), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("13:00-13:29", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("13:00-13-29", "13:00-13-29", "", 127), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("13:30-13:59", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("13:30-13-59", "13:30-13-59", "", 128), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("14:00-14:29", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("14:00-14-29", "14:00-14-29", "", 129), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("14:30-14:59", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("14:30-14-59", "14:30-14-59", "", 130), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("15:00-15:29", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("15:00-15-29", "15:00-15-29", "", 131), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("15:30-15:59", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("15:30-15-59", "15:30-15-59", "", 132), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("16:00-16:29", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("16:00-16-29", "16:00-16-29", "", 133), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("16:30-16:59", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("16:30-16-59", "16:30-16-59", "", 134), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("17:00-17:29", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("17:00-17-29", "17:00-17-29", "", 135), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("17:30-17:59", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("17:30-17-59", "17:30-17-59", "", 136), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("18:00-18:29", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("18:00-18-29", "18:00-18-29", "", 137), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("18:30-18:59", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("18:30-18-59", "18:30-18-59", "", 138), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("19:00-19:29", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("19:00-19-29", "19:00-19-29", "", 139), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("19:30-19:59", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("29:30-19-59", "19:30-19-59", "", 140), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("20:00-20:29", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("20:00-20-29", "20:00-20-29", "", 141), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("20:30-20:59", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("20:30-20-59", "20:30-20-59", "", 142), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("21:00-21:29", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("21:00-21-29", "21:00-21-29", "", 143), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("21:30-21:59", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("21:30-21-59", "21:30-21-59", "", 144), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("22:00-22:29", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("22:00-22-29", "22:00-22-29", "", 145), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("22:30-22:59", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("22:30-22-59", "22:30-22-59", "", 146), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("23:00-23:29", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("23:00-23-29", "23:00-23-29", "", 147), $StatistikDayCat); 
+				}
+				if (!@IPS_GetObjectIDByIdent("23:30-23:59", $StatistikDayCat)) {
+					IPS_SetParent($this->RegisterVariableInteger("23:30-23-59", "23:30-23-59", "", 148), $StatistikDayCat); 
+				}
 			}
-*/	
 		}
 
     	#================================================================================================
